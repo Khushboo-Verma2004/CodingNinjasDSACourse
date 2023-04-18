3957a81ef9bdefe998dbf92da237e0ed8732e82b
Khushboo Verma <116653011+Khushboo-Verma2004@users.noreply.github.com>
2023-04-18 10:36:50 +0530

    Create float.java

---
+++ float.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public static double multiplyFloatingNumbers(double f1, double f2) {
+        // Write your code here
+        float a=(float)f1;
+        float b=(float)f2;
+        return ((f1*f2));
+    }
+}
