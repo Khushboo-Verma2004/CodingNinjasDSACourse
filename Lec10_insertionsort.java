71a5880c469674cfc8303ccb4c0a040455a970ed
Khushboo Verma <116653011+Khushboo-Verma2004@users.noreply.github.com>
2023-06-05 07:33:18 +0530

    Create Lec10_insertionsort.java

---
+++ Lec10_insertionsort.java
@@ -0,0 +1,17 @@
+
+public class Solution {  
+
+    public static void insertionSort(int[] arr) {
+        int n=arr.length;
+    	for(int j=1; j<n; j++){
+            int key=arr[j];
+            int i=j-1;
+            while(i>-1 && arr[i]>key){
+                arr[i+1]=arr[i];
+                i--;
+            }
+            arr[i+1]=key;
+        }   
+    }
+
+}
